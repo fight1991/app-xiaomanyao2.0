52cf2e66b7161ba92b5fde01362435811d340757
fight1991 <359193813@qq.com>
2021-12-02 14:51:25 +0800

    确认解绑页面暂存

--- lib/src/subpages/checkcard/unbind_card.dart
+++ lib/src/subpages/checkcard/unbind_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_car_live/widgets/common_btn/common_btn.dart';
 
 /// @Author: Tiancong
 /// @Date: 2021-12-02 10:19:52
@@ -23,10 +24,51 @@ class _UnbindCardState extends State<UnbindCard> {
       body: Container(
         width: double.infinity,
         padding: EdgeInsets.all(20),
-        child: Column(
-          children: [],
+        child: Flex(
+          direction: Axis.vertical,
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(color: Colors.white),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text(
+                            '卡号',
+                            style: TextStyle(
+                              color: Color(0xff0FB5F9),
+                            ),
+                          ),
+                          trailing: Text(widget.cid),
+                        ),
+                        Divider(height: 1),
+                        ListTile(
+                          title: Text(
+                            '车牌号',
+                            style: TextStyle(
+                              color: Color(0xff0FB5F9),
+                            ),
+                          ),
+                          trailing: Text('1224'),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            CommonBtn(
+              label: '确认解绑',
+              ontap: confirmBtn,
+            )
+          ],
         ),
       ),
     );
   }
+
+  // 确认解绑按钮
+  confirmBtn() {}
 }
