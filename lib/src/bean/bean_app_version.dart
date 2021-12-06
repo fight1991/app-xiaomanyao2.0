a204779260a456c1491cfe4f3e9e99541ec04687
fight1991 <359193813@qq.com>
2021-12-06 17:50:59 +0800

    暂存

--- lib/src/bean/bean_app_version.dart
+++ lib/src/bean/bean_app_version.dart
@@ -1,96 +1,109 @@
+import 'package:json2dart_safe/json2dart.dart';
+
 class AppVersionBean {
-  String? appName;
-  String? appOs;
-  String? buildNum;
-  String? createdBy;
+  int? deleted;
+  int? revision;
+  int? createdBy;
   String? createdByName;
   String? createdTime;
-  String? description;
-  String? downloadUrl;
-  String? frequencyUnit;
-  String? id;
-  String? packageMd5;
-  String? publishDate;
-  String? publishScope;
-  int? remindFrequency;
-  bool? requireInstall;
-  String? status;
-  String? title;
   String? updatedBy;
   String? updatedByName;
   String? updatedTime;
+  String? id;
+  String? appName;
+  String? appOs;
   String? version;
+  String? buildNum;
+  String? downloadUrl;
+  String? title;
+  String? description;
+  String? packageMd5;
+  String? remindFrequency;
+  bool? requireInstall;
+  String? publishDate;
+  String? status;
+  String? publishScope;
+  String? frequencyUnit;
 
-  AppVersionBean(
-      {this.appName,
-      this.appOs,
-      this.buildNum,
-      this.createdBy,
-      this.createdByName,
-      this.createdTime,
-      this.description,
-      this.downloadUrl,
-      this.frequencyUnit,
-      this.id,
-      this.packageMd5,
-      this.publishDate,
-      this.publishScope,
-      this.remindFrequency,
-      this.requireInstall,
-      this.status,
-      this.title,
-      this.updatedBy,
-      this.updatedByName,
-      this.updatedTime,
-      this.version});
-
-  AppVersionBean.fromJson(Map<String, dynamic> json) {
-    appName = json['appName'];
-    appOs = json['appOs'];
-    buildNum = json['buildNum'];
-    createdBy = json['createdBy'];
-    createdByName = json['createdByName'];
-    createdTime = json['createdTime'];
-    description = json['description'];
-    downloadUrl = json['downloadUrl'];
-    frequencyUnit = json['frequencyUnit'];
-    id = json['id'];
-    packageMd5 = json['packageMd5'];
-    publishDate = json['publishDate'];
-    publishScope = json['publishScope'];
-    remindFrequency = json['remindFrequency'];
-    requireInstall = json['requireInstall'];
-    status = json['status'];
-    title = json['title'];
-    updatedBy = json['updatedBy'];
-    updatedByName = json['updatedByName'];
-    updatedTime = json['updatedTime'];
-    version = json['version'];
-  }
+  AppVersionBean({
+    this.deleted,
+    this.revision,
+    this.createdBy,
+    this.createdByName,
+    this.createdTime,
+    this.updatedBy,
+    this.updatedByName,
+    this.updatedTime,
+    this.id,
+    this.appName,
+    this.appOs,
+    this.version,
+    this.buildNum,
+    this.downloadUrl,
+    this.title,
+    this.description,
+    this.packageMd5,
+    this.remindFrequency,
+    this.requireInstall,
+    this.publishDate,
+    this.status,
+    this.publishScope,
+    this.frequencyUnit,
+  });
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['appName'] = this.appName;
-    data['appOs'] = this.appOs;
-    data['buildNum'] = this.buildNum;
-    data['createdBy'] = this.createdBy;
-    data['createdByName'] = this.createdByName;
-    data['createdTime'] = this.createdTime;
-    data['description'] = this.description;
-    data['downloadUrl'] = this.downloadUrl;
-    data['frequencyUnit'] = this.frequencyUnit;
-    data['id'] = this.id;
-    data['packageMd5'] = this.packageMd5;
-    data['publishDate'] = this.publishDate;
-    data['publishScope'] = this.publishScope;
-    data['remindFrequency'] = this.remindFrequency;
-    data['requireInstall'] = this.requireInstall;
-    data['status'] = this.status;
-    data['title'] = this.title;
-    data['updatedBy'] = this.updatedBy;
-    data['updatedByName'] = this.updatedByName;
-    data['updatedTime'] = this.updatedTime;
-    data['version'] = this.version;
-    return data;
+    return Map<String, dynamic>()
+      ..put('deleted', this.deleted)
+      ..put('revision', this.revision)
+      ..put('createdBy', this.createdBy)
+      ..put('createdByName', this.createdByName)
+      ..put('createdTime', this.createdTime)
+      ..put('updatedBy', this.updatedBy)
+      ..put('updatedByName', this.updatedByName)
+      ..put('updatedTime', this.updatedTime)
+      ..put('id', this.id)
+      ..put('appName', this.appName)
+      ..put('appOs', this.appOs)
+      ..put('version', this.version)
+      ..put('buildNum', this.buildNum)
+      ..put('downloadUrl', this.downloadUrl)
+      ..put('title', this.title)
+      ..put('description', this.description)
+      ..put('packageMd5', this.packageMd5)
+      ..put('remindFrequency', this.remindFrequency)
+      ..put('requireInstall', this.requireInstall)
+      ..put('publishDate', this.publishDate)
+      ..put('status', this.status)
+      ..put('publishScope', this.publishScope)
+      ..put('frequencyUnit', this.frequencyUnit);
   }
+
+  AppVersionBean.fromJson(Map<String, dynamic> json) {
+    this.deleted = json.asInt('deleted');
+    this.revision = json.asInt('revision');
+    this.createdBy = json.asInt('createdBy');
+    this.createdByName = json.asString('createdByName');
+    this.createdTime = json.asString('createdTime');
+    this.updatedBy = json.asString('updatedBy');
+    this.updatedByName = json.asString('updatedByName');
+    this.updatedTime = json.asString('updatedTime');
+    this.id = json.asString('id');
+    this.appName = json.asString('appName');
+    this.appOs = json.asString('appOs');
+    this.version = json.asString('version');
+    this.buildNum = json.asString('buildNum');
+    this.downloadUrl = json.asString('downloadUrl');
+    this.title = json.asString('title');
+    this.description = json.asString('description');
+    this.packageMd5 = json.asString('packageMd5');
+    this.remindFrequency = json.asString('remindFrequency');
+    this.requireInstall = json.asBool('requireInstall');
+    this.publishDate = json.asString('publishDate');
+    this.status = json.asString('status');
+    this.publishScope = json.asString('publishScope');
+    this.frequencyUnit = json.asString('frequencyUnit');
+  }
+
+  static AppVersionBean toBean(Map<String, dynamic> json) =>
+      AppVersionBean.fromJson(json);
 }
